587b6caae89a9a84732c4ec8f8637456092f7de7
leriil <leriil@mail.ru>
2019-01-28 16:07:37 +0300

    user changed v-model.lazy, textarea

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,133 @@
 <template>
   <div class="container">
-    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-    <app-new-quote @quoteAdded="newQuote" ></app-new-quote>
-    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <form>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>File a complaint</h1>
+                <hr>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="text"
+                            id="email"
+                            class="form-control"
+                            v-model="userData.email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <!-- lazy modifier will only change the value when the user stopped input -->
+                    <input type="password"
+                            id="password"
+                            class="form-control"
+                             v-model.lazy="userData.password">
+                </div>
+                <div class="form-group">
+                    <label for="age">Age</label>
+                    <input type="number"
+                            id="age"
+                            class="form-control" 
+                            v-model.number="userData.age">
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                <label for="message">Message</label>
+                <!-- interpolation <textarea>{{test}}</textarea> doesn't work! -->
+                <!-- to prepopulate the text field use v-model="messsage" -->
+                <textarea   id="message" 
+                            cols="30" 
+                            rows="5" 
+                            class="form-control" v-model="message"></textarea>
+            </div>
+        </div>
+        <div class="row">
+             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                 <div class="form-group">
+                     <label for="sendmail">
+                         <input type="checkbox"
+                                id="sendmail"
+                                value="SendMail">Send mail      
+                     </label>
+                     <label for="sendInfomail">
+                         <input type="checkbox"
+                                id="sendInfomail"
+                                value="sendInfoMail">Send Infomail      
+                     </label>
+                 </div>
+             </div>
+        </div>
+        <div class="row">
+             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                 <label for="male">
+                     <input type="radio"
+                            id="male"
+                            value="Male">Male
+                 </label>
+                 <label for="female">
+                     <input type="radio"
+                            id="female"
+                            value="Female">Female
+                 </label>
+             </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                <label for="priority">Priority</label>
+                <select id="priority"
+                        class="form-control">
+                        <option></option></select>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <button class="btn btn-primary">
+                    Submit
+                </button>
+            </div>
+        </div>
+    </form>
+    <hr>
     <div class="row">
-      <div class="col sm-12 text-center">
-        <div class="alert alert-info">Click on a quote to delete it</div>
-      </div>
+         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+             <div class="panel panel-default">
+                 <div class="panel-heading">
+                     <h4>Your Data</h4>
+                 </div>
+                 <div class="panel-body">
+                     <p>Mail: {{userData.email}} </p>
+                     <p>Password: {{userData.password}}</p>
+                     <p>Age: {{userData.age}}</p>
+                     <!-- this style will keep the multi-line string -->
+                     <p style="white-space: pre">Message: {{message}} </p>
+                     <p><strong>Send mail? </strong></p>
+                     <ul>
+                         <li></li>
+                     </ul>
+                     <p>Gender: </p>
+                     <p>Priority: </p>
+                 </div>
+             </div>
+         </div>
     </div>
-
-
-
-    <app-table></app-table>
   </div>
 </template>
 
 <script>
-  import Header from './components/Header.vue';
-  import QuoteGrid from './components/QuoteGrid.vue';
-  import NewQuote from './components/NewQuote.vue';
-  import Table from './components/Table.vue';
+  
+  // import Table from './components/Table.vue';
   export default {
-    data:
-      function(){
-        return {
-          maxQuotes: 10,
-          quotes: ['First quote']
+    data(){
+        return{
+            // we bind the property to the input field using v-model
+            userData:{
+                email:'',
+                password:'',
+                age:27
+            }, 
+            message:'A new text'
         }
-      },
-    components:{
-      appHeader: Header,
-      appQuoteGrid:QuoteGrid,
-      appNewQuote: NewQuote,
-      appTable: Table
-    },
-    methods:{
-      newQuote(quote){
-        if(this.quotes.length>=this.maxQuotes){
-            return alert('The quote table is full!');
-        }
-        this.quotes.push(quote);
-        console.log('number of quotes: '+ this.quotes.length);
-      },
-      deleteQuote(index){
-        this.quotes.splice(index,1);
-      }
     }
 }
 </script>
